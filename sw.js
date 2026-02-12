d97c49e54f4f4b57da11b7c0d1e51f17abeaca67
Kooyot <76107993+Kooyot@users.noreply.github.com>
2026-02-12 14:51:12 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,41 +1,5 @@
-const CACHE = 'dm7-v443';
-const URLS = [
-  './',
-  './DIET-MASTER-V7.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => {
-      return Promise.all(
-        URLS.map(url => 
-          fetch(url).then(r => {
-            if (r.ok) cache.put(url, r);
-          }).catch(() => {})
-        )
-      );
-    }).then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    fetch(e.request).then(response => {
-      const clone = response.clone();
-      caches.open(CACHE).then(cache => cache.put(e.request, clone));
-      return response;
-    }).catch(() => caches.match(e.request))
-  );
-});
+const CACHE='dm7-v748';
+const URLS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>Promise.all(URLS.map(u=>fetch(u).then(r=>{if(r.ok)c.put(u,r)}).catch(()=>{})))).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
